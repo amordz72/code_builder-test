baceb5b46a57f9c6a62fdcfad6e98ffc35fe936e
amor <50753427+amordz72@users.noreply.github.com>
2022-01-23 10:06:11 +0100

    fix

--- config/app.php
+++ config/app.php
@@ -55,7 +55,6 @@ return [
     'url' => env('APP_URL', 'http://localhost'),
 
     // 'asset_url' => env('ASSET_URL', null),
-    // 'asset_url' => env('ASSET_URL', null),
     'asset_url' => env("APP_URL"),
     /*
     |--------------------------------------------------------------------------
